8bba8bce8c62adb9b5d06d99441d685b2f7efdc8
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-02-19 09:17:25 +0700

    Add Fail and FailNow testing

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,10 +1,24 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Hanafi")
 	if result != "Hello Hanafi" {
-		panic("Results is not `Hello Hanafi`")
+		t.Fail() // <-- Fail method will continue the code execution
 	}
+
+	fmt.Println("TestHelloWorld has done executed.")
+}
+
+func TestHelloAhmad(t *testing.T) {
+	result := HelloWorld("Ahmad")
+	if result != "Hello Ahmad" {
+		t.FailNow() // <-- FailNow will stop to next code execution
+	}
+
+	fmt.Println("TestHelloAhmad has done executed.")
 }
